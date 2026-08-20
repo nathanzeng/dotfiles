35b6c932cd871c85997f5713aa2a59b611ad2f0c
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-08-20 10:41:22 -0700

    provide some more utilities for the nvim terminal

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -1,7 +1,13 @@
 # NOTE: this is for the terminal within neovim
 
 # Aliases
+alias l='ls -lAF'
 alias gs='git status'
+alias cat='bat --paging=never'
+
+# Use s to jump to the most frecent dir of the given name
+export _Z_CMD="s"
+source ~/.config/bash/z.sh
 
 # Git completion
 source ~/.config/bash/git-completion.bash
